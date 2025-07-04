8cd713647edf533b80b0f6123bc04b84ea362645
Jason Seawall <jseawall@numurus.com>
2025-07-04 10:35:20 -0700

    **

--- scripts/file_pub_pcd_app_node.py
+++ scripts/file_pub_pcd_app_node.py
@@ -76,6 +76,8 @@ class NepiFilePubPcdApp(object):
     'heading_deg': 0,
   }
   
+  node_if = None
+
   paused = False
 
   last_folder = ""
@@ -357,7 +359,8 @@ class NepiFilePubPcdApp(object):
 
     status_msg.running = self.node_if.get_param('running')
 
-    self.node_if.publish_pub('status_pub', status_msg)
+    if self.node_if is not None:
+      self.node_if.publish_pub('status_pub', status_msg)
 
 
   #############################
